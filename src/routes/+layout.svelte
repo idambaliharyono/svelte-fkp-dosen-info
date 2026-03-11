eaf001c327025264d0575bb30850a4902c76208d
idambaliharyono <idambaliharyono01@gmail.com>
2026-03-11 20:54:10 +0800

    applying cards to home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,9 @@
   import unudLogo from "$lib/assets/UNUD (Universitas Udayana) Bali Logo (SVG) - Vector69Com.svg";
   import type { LayoutProps } from "./$types";
   import { page } from "$app/state";
-  import * as Card from "$lib/components/ui/card";
   import { Button } from "$lib/components/ui/button";
+  import Card from "$lib/components/ui/card/card.svelte";
+  import { Bell } from "lucide-svelte";
 
   let { data, children }: LayoutProps = $props();
   let currentPath = $derived(page.url.pathname);
@@ -24,17 +25,20 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="max-w-sm mx-auto border border-black/5 h-screen p-1">
-  <div class="flex">
+<div class="max-w-sm mx-auto border border-black/5 h-screen py-2">
+  <div class="flex justify-between">
     <div class="flex">
-      <img src={unudLogo} alt="" class="size-12 grayscale-100" />
-      <div class="flex flex-col ml-2">
+      <img src={unudLogo} alt="" class="size-15 grayscale-100" />
+      <div class="flex flex-col ml-2 mt-1.5">
         <h1 class="text-xl font-extrabold">SIDATA-IK</h1>
-        <p class="text-sm text-black/80">Prodi Ilmu Kelautan</p>
+        <p class="text-sm text-black/80 -mt-1">Prodi Ilmu Kelautan</p>
       </div>
     </div>
+    <div class="px-3 py-1 flex justify-center my-auto">
+      <Bell class="h-7 w-7" />
+    </div>
   </div>
-  <div class="flex overflow-auto py-3 gap-3">
+  <div class="flex overflow-auto py-3 gap-3 my-2">
     {#each navItems as item}
       <Button href={item.path} variant={getVariant(item.path)}
         >{item.label}</Button
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,8 @@
 <!-- src/routes/test/+page.svelte -->
 <!-- src/routes/test/+page.svelte -->
 <script lang="ts">
+  import Card from "$lib/components/ui/card/card.svelte";
+  import { Car, GroupIcon, Users } from "lucide-svelte";
   import type { PageProps } from "./$types";
   let { data }: PageProps = $props();
 
@@ -26,7 +28,39 @@
   console.log("Keys:", Object.keys(data || {}));
 </script>
 
-<h1>📊 Spreadsheet Data</h1>
-<h1>jumlah dosen: {data.spreadsheet.profil.length}</h1>
-<h1>total publikasi: {data.spreadsheet.karya_ilmiah.length}</h1>
-<h1>total penelitian: {data.spreadsheet.penelitian.length}</h1>
+<Card class="p-3">
+  <h1 class="font-extrabold text-lg">Tracer Data Prodi Ilmu Kelautan</h1>
+  <p class="text-sm text-black/80 -mt-5 text-justify">
+    Monitoring data akademik, mutu, skripsi dan tracer study dalam satu tampilan
+    mobile. User dapat memantau data paling update. Memudahkan proses birokrasi
+    dan komunikasi antara user dan pihak prodi Ilmu Kelautan Fakultas Kelautan
+    dan Perikanan Universitas Udayana
+  </p>
+</Card>
+
+<div class="w-full flex justify-evenly gap-2 mt-3">
+  <Card class="w-1/2 px-3 py-4 flex flex-col">
+    <div class="flex justify-between">
+      <div class="flex flex-col gap-2">
+        <p>Total Dosen:</p>
+        <h1 class="font-extrabold text-xl ml-4">
+          {data.spreadsheet.profil.length}
+        </h1>
+      </div>
+      <Users class="h-10 w-10" />
+    </div>
+    <h2 class="text-xs -mt-2">Data aktif semester ini</h2>
+  </Card>
+  <Card class="w-1/2 px-3 py-4 flex flex-col">
+    <div class="flex justify-between">
+      <div class="flex flex-col gap-2">
+        <p>Total Dosen:</p>
+        <h1 class="font-extrabold text-xl ml-4">
+          {data.spreadsheet.profil.length}
+        </h1>
+      </div>
+      <Users class="h-10 w-10" />
+    </div>
+    <h2 class="text-xs -mt-2">Data aktif semester ini</h2>
+  </Card>
+</div>
